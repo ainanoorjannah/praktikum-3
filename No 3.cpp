0946482448ba622364b429046c5cc7b2129c2c2b
ainanorjannah <ainanoorjannah@gmial.com>
2017-11-08 19:00:33 +0800

    Menghitung dan menampilkan Hasil dari penghitungan gaji karyawan

--- No 3.cpp
+++ No 3.cpp
@@ -6,5 +6,21 @@ int main(){
 	int gp,jk;
 	int lembur,gt,pajak,gk;
 	
+	cout<<"Penghitungan Gaji Karyawan"<<endl;
+	cout<<"Masukkan Gaji Pokok Anda : Rp. ";
+	cin>>gp;
+	cout<<"Masukkan Jam Kerja Anda : Rp. ";
+	cin>>jk;
+	lembur=jk-40;
+	gt=(lembur*1.5+40)*gp;
+	pajak=gt*0.15;
+	gk=gt-pajak;
 	
+	cout<<endl;
+	
+	cout<<"Gaji Pokok 	: Rp."<<gp<<endl;
+	cout<<"Jam Kerja 	: Rp."<<jk<<endl;
+	cout<<"Lembur		: Rp."<<lembur<<endl;
+	cout<<"Gaji Sebelum Dipotong Pajak 	: Rp."<<gt<<endl;
+	cout<<"Total Gaji Anda Adalah		: "<<gk<<endl;
 }
